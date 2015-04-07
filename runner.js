5a3f8126c14b6653192c83404179ad40d0b2ba03
Jason Kraus <zbyte64@gmail.com>
2015-04-07 16:17:26 -0700

    allow passing comparisonResultRoot

--- runner.js
+++ runner.js
@@ -33,6 +33,7 @@ var phantomcss = require(phantomCSSPath+s+'phantomcss.js');
 phantomcss.init({
   screenshotRoot: args.screenshots,
   failedComparisonsRoot: args.failures,
+  comparisonResultRoot: args.results,
   libraryRoot: phantomCSSPath, // Give absolute path, otherwise PhantomCSS fails
 
   onFail: function(test) {
